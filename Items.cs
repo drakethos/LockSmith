9aee9c4a9fb982df221687dcbce43fec5d919724
Derrick Kamphaus <derrick@hauskode.com>
2024-01-18 19:07:03 -0500

    Initial commit

---
+++ Items.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+using Jotunn.Entities;
+using Jotunn.Configs;
+using Jotunn.Entities;
+using Jotunn.GUI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Jotunn.Managers;
+using Jotunn.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Jotunn.Managers;
+using Jotunn.Utils;
+
+namespace LockSmith
+{
+[BepInPlugin("drakemods.VModItem", "Drake VMod - Items", "0.0.1")]
+//[BepInProcess("valheim.exe")]
+[BepInDependency(Jotunn.Main.ModGuid)]
+[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+[BepInDependency("cinnabun.backpacks-v1.0.0", BepInDependency.DependencyFlags.SoftDependency)]
+
+    public class Items : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            
+        PrefabManager.OnVanillaPrefabsAvailable += AddClonedItems;
+
+        }
+
+        private void AddClonedItems()
+        {
+            // Create a custom resource based on Wood
+            ItemConfig customWoodConfig = new ItemConfig();
+            customWoodConfig.Name = "Super Wood";
+            customWoodConfig.Description = "Its super duper wood";
+            customWoodConfig.AddRequirement(new RequirementConfig("Wood", 1));
+            CustomItem recipeComponent = new CustomItem("CustomWood", "Wood", customWoodConfig);
+            ItemManager.Instance.AddItem(recipeComponent);
+
+            // Create and add a custom item based on SwordBlackmetal
+            ItemConfig evilSwordConfig = new ItemConfig();
+            evilSwordConfig.Name = "$item_evilsword";
+            evilSwordConfig.Description = "$item_evilsword_desc";
+            evilSwordConfig.CraftingStation = "piece_workbench";
+            evilSwordConfig.AddRequirement(new RequirementConfig("Stone", 1));
+            evilSwordConfig.AddRequirement(new RequirementConfig("CustomWood", 1));
+
+            CustomItem evilSword = new CustomItem("EvilSword", "SwordBlackmetal", evilSwordConfig);
+            ItemManager.Instance.AddItem(evilSword);
+
+            // You want that to run only once, Jotunn has the item cached for the game session
+            PrefabManager.OnVanillaPrefabsAvailable -= AddClonedItems;
+        }
+
+    }
+}
---
+++ LockSmith.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+using Jotunn.Configs;
+using UnityEngine;
+using Jotunn.Entities;
+using Jotunn.Managers;
+namespace LockSmith
+{
+[BepInPlugin("com.drakemods.LockSmith", "LockSmith", "0.0.1")]
+[BepInProcess("valheim.exe")]
+    public class LockSmith : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("drakesmod.LockSmith");
+        // Your mod's custom localization
+        private CustomLocalization Localization;
+        private void Awake()
+        {
+            
+            harmony.PatchAll();
+            Localizations();
+            makeKeyItems();
+        }
+
+        private void Localizations()
+        {
+            Localization = new CustomLocalization();
+            LocalizationManager.Instance.AddLocalization(Localization);
+            
+            Localization.AddTranslation("English", new Dictionary<string, string>
+            {
+                {"item_setAccessKey", "Give Access Key"}, {"item_setAccessKey_desc", "Use this key on an door or box to allow a player to access it without ward access"},
+                {"locksmith_giveaccess", "Set Access"}, {"locksmith_access_message", "Ready to set, other player touch the object!"},
+                   });
+            
+        }
+
+        private void makeKeyItems()
+        {
+         
+            
+            ItemConfig setAccessKeyConfig = new ItemConfig();
+            setAccessKeyConfig.Name = "$item_setAccessKey";
+            setAccessKeyConfig.Description = "$item_setAccessKey_desc";
+            setAccessKeyConfig.CraftingStation = "piece_workbench";
+            setAccessKeyConfig.AddRequirement(new RequirementConfig("Stone", 1));
+            setAccessKeyConfig.AddRequirement(new RequirementConfig("CustomWood", 1));
+
+            CustomItem evilSword = new CustomItem("EvilSword", "SwordBlackmetal", setAccessKeyConfig);
+            ItemManager.Instance.AddItem(evilSword);
+
+        }
+
+        private void makeItem(string name, ItemConfig itemConfig, string prefab, List<RequirementConfig> requirements)
+        {
+            makeItem(name, itemConfig.Name, itemConfig.Description, prefab, requirements, itemConfig.CraftingStation);
+        }
+
+        private void makeItem(string name, string gameName, string description, string prefab,
+            List<RequirementConfig> requirements, string craftingStation = "piece_workbench")
+        {
+            ItemConfig itemConfig = new ItemConfig();
+            itemConfig.Name = gameName;
+            itemConfig.Description = description;
+            itemConfig.CraftingStation = craftingStation;
+            foreach (var requirement in requirements)
+            {
+                itemConfig.AddRequirement(requirement);
+
+
+                CustomItem customItem = new CustomItem(name, prefab, itemConfig);
+                ItemManager.Instance.AddItem(customItem);
+            }
+        }
+
+        [HarmonyPatch(typeof(Container),"CheckAccess" )]
+        class Private_mod
+        {
+            static void Postfix(ref Container.PrivacySetting ___m_privacy)
+            {
+         
+                ___m_privacy = Container.PrivacySetting.Private;
+                Debug.Log($"Modified jump force: {___m_privacy}");
+                if (___m_privacy == Container.PrivacySetting.Private)
+                {
+                    Debug.Log($"privacy set to: {___m_privacy}");
+                }
+            }
+
+            
+        }
+    }
+}
